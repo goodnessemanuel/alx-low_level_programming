65bd258b450539d27cedbd2004e3c5c0980921b3
goodnessemanuel <goodnessemmanuel.ge@gmail.com>
2022-09-08 23:40:40 -0700

    c programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+/**
+ * Main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("With proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
