84cce847d97bb9666b0101acc46b7b3427f3f8dd
goodnessemanuel <goodnessemmanuel.ge@gmail.com>
2022-09-10 12:35:48 -0700

    statements

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
