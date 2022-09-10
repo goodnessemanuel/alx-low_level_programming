3b4c1b4f7609b03c7bc16cac029075256e80ca11
goodnessemanuel <goodnessemmanuel.ge@gmail.com>
2022-09-10 11:35:20 -0700

    variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
