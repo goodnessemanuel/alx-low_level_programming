5f7df0400d0e94e048eb99f8c114934dfbc59b7b
goodnessemanuel <goodnessemmanuel.ge@gmail.com>
2022-09-13 14:48:09 -0700

    functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * main - Functions, nested loops
+ * description: It prints the word Putchar, folloed by a new line.
+ * Return: 0.
+ */
+int main(void)
+{
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		putchar(word[i]);
+	putchar('\n');
+	return (0);
+}
